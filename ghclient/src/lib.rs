9a73c6b21d84b138474e5b01525a46abbc973389
Martin Rueckl <martin.rueckl@codecentric.de>
2022-11-17 18:04:11 +0100

    separated GH client into separate crate and transition to surf client

--- ghclient/src/lib.rs
+++ ghclient/src/lib.rs
@@ -1,10 +1,8 @@
 use serde::Deserialize;
-// use gloo_net::http::{Method, Request};
 use regex::Regex;
-use surf::{Client, Request, Response};
+use surf::{Client, Request, Response, StatusCode};
 use surf::http::Method;
-
-// use web_sys::RequestMode;
+use anyhow::{anyhow, Result};
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct GHUser {
@@ -25,6 +23,8 @@ pub struct GHClient {
     client: Client,
 }
 
+// FIXME: turn this into a trait and provide blanket implementations for tower::Service<HTTPRequest>
+//        see: https://docs.rs/tower-http/latest/tower_http/index.html#example-client
 impl GHClient {
     pub fn new(client: Client, token: Option<String>) -> Self { Self { client, token } }
 
@@ -32,6 +32,8 @@ impl GHClient {
     fn request(&self, method: Method, url: &str) -> Request {
         let mut request = Request::new(method, url.try_into().unwrap());
         request.set_header("Accept", "application/vnd.github.v3+json");
+        // fixme: rework the header injection via the tower_http service layers.
+        request.set_header("User-Agent", "Awesome-Octocat-App");
 
         // any nice functional way to incorporate this in the above builder pattern?
         if let Some(token) = &self.token {
@@ -42,19 +44,21 @@ impl GHClient {
 
     /// extract the last page number from the response headers.
     /// Note: Pagination is "one based" - I.e. a range 1..last_page + 1 in the GH API.
-    fn last_page(response: Response) -> Result<usize, String> {
-
+    fn last_page(response: Response) -> Result<usize> {
         // Note: in the API query parameters aren't zero based!
         match response.header("link") {
             Some(pagination_header) => {
                 log::debug!("pagination header: {pagination_header}");
-                let re = Regex::new(r".*?&page=([0-9]+).*").unwrap();
+                let re = Regex::new(r".*&page=([0-9]+).*").expect("Failed to construct regex");
                 let last_page: usize = re
                     .captures_iter(&pagination_header.to_string())
+                    // .map(|c| {
+                    //     println!("match: {c:?}");
+                    //     c
+                    // })
                     .last()
-                    .ok_or("Could not parse pagination header".to_string())?[1]
-                    .parse::<usize>()
-                    .map_err(|e| e.to_string())?;
+                    .ok_or(anyhow!("Could not parse pagination header"))?[1]
+                    .parse::<usize>()?;
                 log::debug!("last page from header: {last_page}");
                 Ok(last_page)
             }
@@ -66,20 +70,20 @@ impl GHClient {
     }
 
     /// Get a single page of organization members.
-    async fn get_org_members_page(&self, org: &str, page: usize) -> Result<Vec<GHUser>, String> {
+    async fn get_org_members_page(&self, org: &str, page: usize) -> Result<Vec<GHUser>> {
         log::debug!("fetching {org}-org member page {page}");
         let request = self.request(
             Method::Get,
             &format!("https://api.github.com/orgs/{org}/members?per_page=30&page={page}"),
         );
 
-        let mut response = self.client.send(request).await.map_err(|e| format!("Failed to send request: {e}"))?;
-        let members: Vec<GHUser> = response.body_json().await.map_err(|e| format!("Failed to parse Users from body: {e}"))?;
+        let mut response = self.client.send(request).await.map_err(|e| anyhow!("Failed sending request: {e:?}"))?;
+        let members: Vec<GHUser> = response.body_json().await.map_err(|e| anyhow!("Failed reading body: {e:?}"))?;
         Ok(members)
     }
 
     /// Get all members of an organization.
-    pub async fn get_org_members(&self, org: &str) -> Result<Vec<GHUser>, String> {
+    pub async fn get_org_members(&self, org: &str) -> Result<Vec<GHUser>> {
         log::info!("fetching organization members of {org}");
 
         let request = self.request(
@@ -89,12 +93,10 @@ impl GHClient {
 
         // get the link header
         // link: <.../{org}/members?page=2>; rel="next", <...{org}/members?page=123>; rel="last"
-        let response = self.client.send(request)
-            .await
-            .map_err(|e| format!("failed to fetch page count: {e}"))?;
+        let response = self.client.send(request).await.map_err(|e| anyhow!("Failed sending request: {e:?}"))?;
 
         // Note: in the API query parameters aren't zero based!
-        let last_page: usize = GHClient::last_page(response).map_err(|e| format!("Failed to get page count: {e}"))?;
+        let last_page: usize = GHClient::last_page(response)?;
 
         let mut users: Vec<GHUser> = Vec::with_capacity((last_page - 1) * 30);
 
@@ -109,19 +111,22 @@ impl GHClient {
     }
 
     /// Get a single page of the repositories of a user.
-    async fn get_user_repositories_page(&self, user: &str, page: usize) -> Result<Vec<GHRepository>, String> {
+    async fn get_user_repositories_page(&self, user: &str, page: usize) -> Result<Vec<GHRepository>> {
         log::debug!("fetching {user} repository page {page}");
         let request = self.request(
             Method::Get,
             &format!("https://api.github.com/users/{user}/repos?per_page=30&page={page}"),
         );
-        let mut response = self.client.send(request).await.map_err(|e| format!("Failed to send request: {e}"))?;
+        let mut response = self.client.send(request).await.map_err(|e| anyhow!("Failed sending request: {e:?}"))?;
+        if response.status() != StatusCode::Ok {
+            return Err(anyhow!("Request failed with {}", response.status()));
+        }
 
-        let repos: Vec<GHRepository> = response.body_json().await.map_err(|e| e.to_string())?;
+        let repos: Vec<GHRepository> = response.body_json().await.map_err(|e| anyhow!("Failed reading body: {e:?}"))?;
         Ok(repos)
     }
 
-    pub async fn get_user_repositories(&self, user: &str) -> Result<Vec<GHRepository>, String> {
+    pub async fn get_user_repositories(&self, user: &str) -> Result<Vec<GHRepository>> {
         log::info!("fetching user repositories for {user}");
 
         let request = self.request(
@@ -131,8 +136,8 @@ impl GHClient {
 
         // get the link header
         // link: <.../{org}/members?page=2>; rel="next", <...{org}/members?page=123>; rel="last"
-        let response = self.client.send(request).await.map_err(|e| e.to_string())?;
-        let last_page: usize = GHClient::last_page(response).map_err(|e| format!("Failed to get page count: {e}"))?;
+        let response = self.client.send(request).await.map_err(|e| anyhow!("Failed sending request: {e:?}"))?;
+        let last_page: usize = GHClient::last_page(response)?;
 
         let mut repos: Vec<GHRepository> = Vec::with_capacity(last_page * 30);
         // concurrent fetching of pages
@@ -149,23 +154,44 @@ impl GHClient {
 #[cfg(test)]
 mod tests {
     use surf::Client;
-    use crate::gh_api::GHClient;
+    use crate::GHClient;
+    use rstest::*;
+    use anyhow::Result;
 
-    #[test]
-    async fn test_get_repos_single_page() {
-        let client = GHClient::new(Client::new(), None);
-        let repos = client.get_user_repositories("maiksensi").await;
-        assert!(repos.is_ok());
-        // make sure below number matches with https://github.com/maiksensi
-        assert_eq!(29, repos.unwrap().len());
+    #[fixture]
+    fn token() -> String {
+        dotenv::dotenv().ok();
+        std::env::var("GH_API_TOKEN").expect("Failed to load github token from environment variables.")
     }
 
-    #[test]
-    async fn test_get_repos_multi_page() {
-        let client = GHClient::new(Client::new(), None);
-        let repos = client.get_user_repositories("jonashackt").await;
-        assert!(repos.is_ok());
+    #[rstest]
+    #[tokio::test]
+    async fn test_get_repos_page(token: String) -> Result<()> {
+        // let http_client: Client = surf::Config::new()
+        //     // fixme: this here seems to get ignored in the GHClient, because it builds
+        //     //        its own request and does not merge the header. Better use tower_http as
+        //     //        client abstraction in the future, given there is an wasm compatible
+        //     //        service implementation.
+        //     // .add_header("User-Agent", "Awesome-Octocat-App")
+        //     .unwrap()
+        //     .try_into()
+        //     .unwrap();
+        let client = GHClient::new(Client::new(), Some(token));
+        let repos = client.get_user_repositories("maiksensi").await?;
+        println!("repos: {repos:?}");
+        // make sure below number matches with https://github.com/maiksensi
+        assert_eq!(29, repos.len());
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_get_repos_multi_page(token: String) -> Result<()> {
+        let client = GHClient::new(Client::new(), Some(token));
+        let repos = client.get_user_repositories("jonashackt").await?;
+        println!("repos: {repos:?}");
         // make sure below number matches with https://github.com/jonashackt
-        assert_eq!(145, repos.unwrap().len());
+        assert_eq!(145, repos.len());
+        Ok(())
     }
 }
